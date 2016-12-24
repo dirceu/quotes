fb751e84af26a411dadedf3520704c2af769777a
Dirceu Pereira Tiegs <dirceutiegs@gmail.com>
2016-12-24 19:41:23 -0200

    Fix scaling issue in mobile devices

--- pages/index.js
+++ pages/index.js
@@ -1129,27 +1129,32 @@ export default class MyPage extends React.Component {
   render () {
     return (
       <div style={{ textAlign: 'right', padding: '5%' }}>
-        <style jsx>{`
-          @import url('https://fonts.googleapis.com/css?family=Montserrat:700');
-          * {
-            font-family: 'Montserrat', sans-serif;
-            font-weight: bold;
-            background-color: ${this.props.color};
-            color: #FFFFFF;
-          }
-          blockquote {
-            font-size: 4vw;
-            padding: 0;
-          }
-          cite {
-            font-size: 2.8vw;
-            padding: 0;
-            padding-right: 5%;
-          }
-          small {
-            display: none;
-          }
-        `}</style>
+        <Head>
+          <title>Random quote from https://github.com/dirceu/codex-vitae/blob/master/CommonplaceBook.md</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta charset="utf-8" />
+          <style jsx>{`
+            @import url('https://fonts.googleapis.com/css?family=Montserrat:700');
+            * {
+              font-family: 'Montserrat', sans-serif;
+              font-weight: bold;
+              background-color: ${this.props.color};
+              color: #FFFFFF;
+            }
+            blockquote {
+              font-size: 4vw;
+              padding: 0;
+            }
+            cite {
+              font-size: 2.8vw;
+              padding: 0;
+              padding-right: 5%;
+            }
+            small {
+              display: none;
+            }
+          `}</style>
+        </Head>
         <blockquote>{this.props.quote.contents}</blockquote>
         <cite>{this.props.quote.author}</cite>
         <small>by <a href='https://twitter.com/dirceu' rel='me'>@dirceu</a></small>
